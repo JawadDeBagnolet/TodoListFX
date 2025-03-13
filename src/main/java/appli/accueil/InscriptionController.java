a91565a8f5851616d2596648b4a25cf6f8e83a0a
JawadDeBagnolet <cjawad.a@gmail.com>
2025-03-13 15:14:41 +0100

    Etape 8

--- src/main/java/appli/accueil/InscriptionController.java
+++ src/main/java/appli/accueil/InscriptionController.java
@@ -1,10 +1,13 @@
 package appli.accueil;
 
+import appli.StartApplication;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 public class InscriptionController {
     @FXML
@@ -30,45 +33,71 @@ public class InscriptionController {
         System.out.println("Prenom :  " + prenomField.getText());
         System.out.println("Email :  " + emailField.getText());
         System.out.println("Mot de Passe :  " + mdpField.getText());
-        connectedText.setText("Vous voilà Inscrit !");
+
+        if (confirmationMdpField.getText().equals(mdpField.getText())) {
+            connectedText.setText("Vous voilà Inscrit !");
+        } else {
+            connectedText.setText("Erreur : Les deux mot de passe sont différents !");
+        }
+        if (emailField.getText().equals("email")){
+            connectedText.setText("Erreur : Email déjà utilisé !");
+        }
+
+        if (confirmationMdpField.getText().isEmpty()||nomField.getText().isEmpty()||prenomField.getText().isEmpty() || emailField.getText().isEmpty() || mdpField.getText().isEmpty()){
+            connectedText.setText("Erreur : Champ(s) vide(s) !");
+        }
     }
 
+    @FXML
+    void redirectionConnexion() throws IOException {
+        StartApplication.changeScene("accueil/Login");
+    }
+    @FXML
     public TextField getPrenomField() {
         return prenomField;
     }
 
+    @FXML
     public void setPrenomField(TextField prenomField) {
         this.prenomField = prenomField;
     }
 
+    @FXML
     public TextField getNomField() {
         return nomField;
     }
 
+    @FXML
     public void setNomField(TextField nomField) {
         this.nomField = nomField;
     }
 
+    @FXML
     public TextField getMdpField() {
         return mdpField;
     }
 
+    @FXML
     public void setMdpField(TextField mdpField) {
         this.mdpField = mdpField;
     }
 
+    @FXML
     public TextField getConfirmationMdpField() {
         return confirmationMdpField;
     }
 
+    @FXML
     public void setConfirmationMdpField(TextField confirmationMdpField) {
         this.confirmationMdpField = confirmationMdpField;
     }
 
+    @FXML
     public TextField getEmailField() {
         return emailField;
     }
 
+    @FXML
     public void setEmailField(TextField emailField) {
         this.emailField = emailField;
     }
