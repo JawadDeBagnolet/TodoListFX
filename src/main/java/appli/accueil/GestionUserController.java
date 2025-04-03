44f1dbfca4623f05d1603c6ce8229ef78c793c2f
JawadDeBagnolet <cjawad.a@gmail.com>
2025-04-03 16:50:56 +0200

    gestion user

---
+++ src/main/java/appli/accueil/GestionUserController.java
@@ -0,0 +1,19 @@
+package appli.accueil;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import model.Utilisateur;
+
+import javax.swing.text.TableView;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GestionUserController implements Initializable {
+    @FXML
+    private TableView tableauUser;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
